240034121d9b0ac56df13de1ac1f1b760045dab7
Sam Stephenson <sam@37signals.com>
2010-02-21 21:32:42 -0600

    Just use super since I didn't end up doing anything with the block after all

--- test/lib/hector/test_case.rb
+++ test/lib/hector/test_case.rb
@@ -6,14 +6,11 @@ module Hector
       define_method("test #{name.inspect}", &block)
     end
 
-    def run_with_logging(result, &block)
+    def run(*)
       Hector.logger.info "--- #@method_name ---"
-      run_without_logging(result, &block)
+      super
       Hector.logger.info " "
     end
-
-    alias_method :run_without_logging, :run
-    alias_method :run, :run_with_logging
 
     def connection
       Hector::TestConnection.new("test")
