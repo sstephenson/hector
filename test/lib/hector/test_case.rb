4a5b5f3f6ffc9a1ce598e007bf0ff657f5cc15a5
Sam Stephenson <sam@37signals.com>
2010-02-21 21:09:55 -0600

    Add test logging

--- test/lib/hector/test_case.rb
+++ test/lib/hector/test_case.rb
@@ -6,6 +6,15 @@ module Hector
       define_method("test #{name.inspect}", &block)
     end
 
+    def run_with_logging(result, &block)
+      Hector.logger.info "--- #@method_name ---"
+      run_without_logging(result, &block)
+      Hector.logger.info " "
+    end
+
+    alias_method :run_without_logging, :run
+    alias_method :run, :run_with_logging
+
     def connection
       Hector::TestConnection.new("test")
     end
