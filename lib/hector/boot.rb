0f194260f8db248a5f10657e21368d802fd3d28a
Sam Stephenson <sam@37signals.com>
2010-03-07 21:46:54 -0600

    bin/hector-setup and Hector.init_app

--- lib/hector/boot.rb
+++ lib/hector/boot.rb
@@ -4,6 +4,22 @@ require "logger"
 module Hector
   class << self
     attr_accessor :lib, :root
+
+    def init_app
+      set_root!
+      load Hector.root.join("init.rb")
+    end
+
+    def set_root!
+      if root = ENV["HECTOR_ROOT"]
+        Hector.root = Pathname.new(File.expand_path(root))
+      else
+        dir = Pathname.new(Dir.pwd)
+        dir = dir.parent while dir != dir.parent && dir.basename.to_s !~ /\.hect$/
+        abort "error: please specify HECTOR_ROOT" if dir == dir.parent
+        Hector.root = dir
+      end
+    end
   end
 
   self.lib = Pathname.new(File.dirname(__FILE__) + "/..")
@@ -20,11 +36,3 @@ rescue LoadError => e
     raise e
   end
 end
-
-Hector.root = Pathname.new(ENV["HECTOR_ROOT"] || Hector.lib.join(".."))
-Hector.logger = Logger.new($stderr)
-Hector.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
-
-Hector::Identity.adapter = Hector::YamlIdentityAdapter.new(
-  Hector.root.join("config/identities.yml")
-)
