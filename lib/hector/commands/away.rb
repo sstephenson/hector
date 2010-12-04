fa563f32973d368735f8224917d2b7dc448240eb
Sam Stephenson <sam@37signals.com>
2010-12-04 10:24:35 -0600

    Style

--- lib/hector/commands/away.rb
+++ lib/hector/commands/away.rb
@@ -5,10 +5,10 @@ module Hector
         away_message = request.args.first
         if away_message and !away_message.empty?
           @away_message = away_message
-          respond_with("306", {:text => "You have been marked as being away"})
+          respond_with("306", :text => "You have been marked as being away")
         else
           @away_message = nil
-          respond_with("305", {:text => "You are no longer marked as being away"})
+          respond_with("305", :text => "You are no longer marked as being away")
         end
       end
     end
