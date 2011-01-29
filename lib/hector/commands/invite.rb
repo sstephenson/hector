9b3ce800173d633526af9c7715e627e2da4443fd
Clint Ecker <me@clintecker.com>
2011-01-28 23:39:46 -0600

    cleaning up the queries

--- lib/hector/commands/invite.rb
+++ lib/hector/commands/invite.rb
@@ -5,16 +5,15 @@ module Hector
         touch_presence
         nickname = request.args.first
         if session = Session.find(nickname)
-          channel_name = request.args[1]
-          channel = Channel.find(channel_name)
-          if Channel.find_all_for_session(self).include?(channel)
-            if !Channel.find_all_for_session(session).include?(channel)
+          channel = Channel.find(request.args[1])
+          if channels.include?(channel)
+            if !session.channels.include?(channel)
               session.deliver(:invite, self, :source => source, :text => request.text)
             else
-              respond_with("443", nickname, channel_name, "is already on channel", :source => Hector.server_name)
+              respond_with("443", nickname, channel.name, "is already on channel", :source => Hector.server_name)
             end
           else
-            respond_with("442", channel_name, "You're not on that channel", :source => Hector.server_name)
+            respond_with("442", request.args[1], "You're not on that channel", :source => Hector.server_name)
           end
         else
           raise NoSuchNickOrChannel, nickname
