f320d3d363e34382924e117ad04d4ec1c0ce05ad
leedo <lee@laylward.com>
2011-02-01 18:35:06 -0600

    accept a regexp for assert_not_sent_to

--- test/lib/hector/integration_test.rb
+++ test/lib/hector/integration_test.rb
@@ -69,7 +69,7 @@ module Hector
 
     def assert_not_sent_to(connection, line, &block)
       sent_data = block ? capture_sent_data(connection, &block) : connection.sent_data
-      assert sent_data !~ /^#{Regexp.escape(line)}/
+      assert sent_data !~ /^#{line.is_a?(Regexp) ? line : Regexp.escape(line)}/
     end
 
     def assert_nothing_sent_to(connection, &block)
