ea11a636b533e49af0fcb7d77f783dd16ac8a656
Ross Paffett <ross@rosspaffett.com>
2012-02-01 20:31:21 -0500

    Include originating server name as first parameter in PONG responses, as per RFC 2812

--- lib/hector/commands/ping.rb
+++ lib/hector/commands/ping.rb
@@ -2,7 +2,7 @@ module Hector
   module Commands
     module Ping
       def on_ping
-        respond_with(:pong, :source => Hector.server_name, :text => request.text)
+        respond_with(:pong, Hector.server_name, :source => Hector.server_name, :text => request.text)
       end
     end
   end
